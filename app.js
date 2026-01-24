011d7020f1c3935f15daa41e71e916c1dcf6ae2f
Kavindutharaka <ktliyanapathirana2000@gmail.com>
2026-01-24 18:33:49 +0530

    UI Updateed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('FitnessApp', []);
 app.controller('MainCtrl', function ($scope, $interval, $timeout, $http) {
     console.log("App Started!");
 
-    $scope.page = 2;
+    $scope.page = 5;
 
     $scope.pg_up = function(){
         $scope.page ++;
